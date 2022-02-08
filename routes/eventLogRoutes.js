57e8dbd6bd1cfe6004c574fa3968ddf1de78e463
Sameer <sameerad2001@gmail.com>
2022-02-08 13:22:11 +0530

    Added routes to show location frequency and user created events

--- routes/eventLogRoutes.js
+++ routes/eventLogRoutes.js
@@ -37,4 +37,69 @@ module.exports = (app) => {
                     res.send("Recorded a new event")
             })
         });
+
+    // Returns all the events that a particular user created
+    app.route("/userDetails/:userID")
+        .get((req, res) => {
+
+            EventLog.find({ userID: req.params.userID }, (err, eventsByUser) => {
+                if (err)
+                    res.send(err)
+                else
+                    res.send(eventsByUser);
+            }
+            )
+
+        });
+
+
+    // Number of events logged by the user
+    app.route("/userFrequency")
+        .get((req, res) => {
+
+            EventLog.aggregate([
+                {
+                    $group: {
+                        _id: '$userID',
+                        numberOfUsers: { $sum: 1 }
+                    }
+                },
+                {
+                    $project: {
+                        userID: '$_id',
+                        numberOfUsers: 1,
+                        _id: 0
+                    }
+                }
+            ]).then((result) => {
+                res.send(result);
+            })
+
+
+        });
+
+    // Find the number of user's from a given location
+    app.route("/locationFrequency")
+        .get((req, res) => {
+
+            EventLog.aggregate([
+                {
+                    $group: {
+                        _id: '$location',
+                        numberOfUsers: { $sum: 1 }
+                    }
+                },
+                {
+                    $project: {
+                        location: '$_id',
+                        numberOfUsers: 1,
+                        _id: 0
+                    }
+                }
+            ]).then((result) => {
+                res.send(result);
+            })
+
+
+        })
 }
